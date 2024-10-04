c2cd1abe4cbebfa38411e7bd3d32006eb71c4b69
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-10-04 15:34:58 +0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx"
+import { defineConfig } from "astro/config"
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap"
+
+import cloudflare from "@astrojs/cloudflare"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+	site: "https://groups12.ru",
+	integrations: [mdx(), sitemap(), tailwind(), react()],
+
+	vite: {
+		build: {
+			minify: true,
+		},
+	},
+
+	output: "hybrid",
+	adapter: cloudflare(),
+})
